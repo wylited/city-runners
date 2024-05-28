8e6e5f842de0116621733e761ed62bb014ff615c
wylited <44154103+wylited@users.noreply.github.com>
2024-05-28 19:03:36 +0800

    switch to flutter

--- web/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import { wsconnect, sendLocation } from './ws';
-
-export { wsconnect, sendLocation };
--- web/src/main.js
+++
@@ -1,3 +0,0 @@
-import { wsconnect } from '$lib';
-
-wsconnect();
